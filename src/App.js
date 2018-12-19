97764bb5db256831ec11a559755e5bc8be2ea39e
KangGilHwan <rlfghksop@naver.com>
2018-12-20 04:27:40 +0900

    Add reducer and action

--- src/App.js
+++ src/App.js
@@ -10,63 +10,11 @@ const colors = ['#343a40', '#f03e3e', '#12b886', '#228ae6'];
 
 class App extends Component {
 
-  // id = 0
-  //
-  // state = {
-  //   input: '',
-  //   color: '#343a40',
-  //   todos: []
-  // }
-
-  // handleChange = (e) => {
-  //   this.setState({
-  //     input: e.target.value
-  //   });
-  // }
-
-  // handleCreate = () => {
-  //   const { input, todos, color } = this.state;
-  //   this.setState({
-  //     input: '',
-  //     todos: todos.concat({
-  //       id: this.id++,
-  //       text: input,
-  //       checked: false,
-  //       color: color
-  //     })
-  //   });
-  // }
-
   handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       this.props.handleCreate();
     }
   }
-
-  // handleToggle = (id) => {
-  //   const { todos } = this.state;
-  //
-  //   const index = todos.findIndex(todo => todo.id === id);
-  //   const selected = todos[index];
-  //
-  //   const nextTodos = [...todos];
-  //
-  //   nextTodos[index] = {
-  //     ...selected,
-  //     checked: !selected.checked
-  //   };
-  //
-  //   this.setState({
-  //     todos: nextTodos
-  //   });
-  // }
-
-  // handleRemove = (id) => {
-  //   const {todos} = this.state;
-  //   this.setState({
-  //     todos: todos.filter(todo => todo.id !== id)
-  //   });
-  // }
 
   changeColor = (color) => {
 
@@ -77,23 +25,22 @@ class App extends Component {
 
   render() {
     const { input, todos, color } = this.props;
+    const { changeColor, handleKeyPress } = this;
     const {
-      //handleChange,
-    //  handleCreate,
-      handleKeyPress,
-    //  handleToggle,
-    //  handleRemove,
-      changeColor
-    } = this;
+      handleChange,
+      handleCreate,
+      handleToggle,
+      handleRemove
+    } = this.props;
 
     return (
       <TodoListTemplate form={
-        <Form value={input} onKeyPress={handleKeyPress} onChange={this.props.handleChange} onCreate={this.props.handleCreate} color={color}/>
+        <Form value={input} onKeyPress={handleKeyPress} onChange={handleChange} onCreate={handleCreate} color={color}/>
       }
       palette={(
           <Palette colors={colors} selected={color} onSelect={changeColor}/>
       )}>
-        <TodoItemList todos={todos} onToggle={this.props.handleToggle} onRemove={this.props.handleRemove}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </TodoListTemplate>
     );
   }
